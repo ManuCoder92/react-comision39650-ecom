1ef6d9ff17383c875c425c65efd08679ee0851c4
Juan Manuel Castillo <jmkasti11@gmail.com>
2023-03-28 23:05:36 +0200

    cambios2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./Componete";
+import "./Navbar";
 
 function App() {
   return (
     <div className="App">
+      <div>
+     
+        </div> 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
